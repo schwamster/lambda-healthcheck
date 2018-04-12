427f5d19716005b56f650ff9ac4c212a09285baa
schwamster <bastian.toepfer@gmail.com>
2018-04-12 07:15:40 +0200

    send of hc result to sqs

--- handler.js
+++ handler.js
@@ -1,28 +1,70 @@
 'use strict';
 const axios = require('axios');
+const log = require('lambda-log');
+const AWS = require('aws-sdk');
+const zlib = require('zlib');
+AWS.config.update({ region: 'us-west-2' });
+var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 module.exports.healthcheck = (event, context, callback) => {
-  
-  axios.get(event.service.url)
-    .then(response => {
-      if(response.data.app != event.service.app){
-        callback(null, {statusCode: 500, error: `the app name did not match the expected app name. expected ${event.service.app}, actual ${response.data.app}`, input: event});  
+
+  log.config.meta.app = event.service.app;
+  log.config.meta.version = event.service.version;
+  log.config.meta.url = event.service.url;
+  log.config.tags.push('dev');
+
+  log.on('log', function (data) {
+    let params = {
+      DelaySeconds: 10,
+      MessageAttributes: {
+        "Type": {
+          DataType: "String",
+          StringValue: "Healthcheck"
+        }
+      },
+      MessageBody: prepareMessageContent(JSON.stringify(data)),
+      QueueUrl: "https://sqs.eu-west-1.amazonaws.com/394296847239/healthcheck-queue"
+    };
+
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.MessageId);
       }
-      else if(response.data.version != event.service.version){
-        callback(null, {statusCode: 500, error: `the app version did not match the expected app version. expected ${event.service.version}, actual ${response.data.version}`, input: event});  
-      }
-      else{
-        callback(null, {statusCode: 200, body: response.data, input: event});
-      }
-    })
-    .catch(error => { 
-      console.log(error);
-      callback(null, {statusCode: 500, error: error});
     });
 
+  });
 
-  //callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  axios.get(event.service.url)
+    .then(response => {
+      if (response.data.app != event.service.app) {
+        let msg = `the app name did not match the expected app name. expected ${event.service.app}, actual ${response.data.app}`;
+        log.info(`Healthcheck NOK [${msg}]`);
+        callback(null, { statusCode: 500, error: msg, input: event });
+      }
+      else if (response.data.version != event.service.version) {
+        let msg = `the app version did not match the expected app version. expected ${event.service.version}, actual ${response.data.version}`
+        log.info(`Healthcheck NOK [${msg}]`);
+        callback(null, { statusCode: 500, error: msg, input: event });
+      }
+      else {
+        log.info("Healthcheck OK");
+        callback(null, { statusCode: 200, body: response.data, input: event });
+      }
+    })
+    .catch(error => {
+      log.info(error);
+      callback(null, { statusCode: 500, error: error });
+    });
 };
+
+function prepareMessageContent(message){
+  var compressed = zlib.gzipSync(message).toString('base64');
+  return compressed;
+}
+
+module.exports.prepareMessageContent = (message) => {
+
+  return prepareMessageContent(message);
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var healthcheck = require('../handler')
 
-const backendVersion = '0.1.0-783';
+const backendVersion = '0.1.0-820';
 const backendUrl = 'https://maintenance-backend.sws-fieldtest.prod.aws.scania.com/healthcheck';
 
 describe('Array', function () {
@@ -59,6 +59,17 @@ describe('Array', function () {
                     done(error);
                 }
             });
+        });
+
+        it('should prepare the message as planned', function () {
+
+            let msg = '{"app":"sws-backend-maintainance-service","timestamp":"2017-02-23T13:49:21.1437670Z","message":"request to \\\"/api/Job/6789\\\" took 1ms","level":"Information","operation":"/api/Job/6789","duration":1,"category":"performance","correlationId":"0HL2RRRM7EAD0","threadId":22,"user":"2"}';
+            
+            let expected = 'H4sIAAAAAAAAA1WPQUvEMBCF/0qZc2OTdNm4vQkKruileJK9zKbjGrZJapKuiPjfnVY8eBgY3vveMO8LcJqgg/yRxRHtmcIgPLpQeDBYEpnSxVmCGorzlAv6BddSGSG10O2zarvNrtPqSm1aszXyhVEGM56IwUTvM6eqEqsDNDi55iEem6253h2AxXiulM+cGOlCI/P78BqTx+JiYDVOlH737n+YvWH+s1QNFgudYvpkjiPribA+bWNKNK7gfmBX3j/qvu+fzN3NrVxKvSXCYbG0rmHmtks7+P4BEasl5RgBAAA=';
+            var result = healthcheck.prepareMessageContent(msg);
+
+            assert.equal(result, expected);
+            
         });
     });
 });
